44db036b273a0acf2d2f59113beab34a4319f4da
tttttandon <t.champion@tcu.edu>
2026-09-21 10:18:46 -0500

    updated app.js with functions, DOM, and Events

--- js/app.js
+++ js/app.js
@@ -23,3 +23,20 @@ console.log(typeof myFavorite.category);
 console.log(typeof myFavorite.rating);
 console.log(typeof myFavorite.notes);
 console.log(typeof myFavorite.dateAdded);
+
+function greetFavorite(placeName, rating) {
+    console.log(placeName + ' has ' + rating + ' stars!');
+}
+greetFavorite('Starbucks', 5);   // "Starbucks has 5 stars!"
+
+const nameInput = document.getElementById('name');
+console.log(nameInput.value);   // what the user typed
+
+const practiceForm = document.getElementById('add-favorite-form');
+
+function handleSubmit(event) {
+    event.preventDefault();   // stop the page reload
+    console.log('You typed: ' + nameInput.value);
+}
+
+practiceForm.addEventListener('submit', handleSubmit);
